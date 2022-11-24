1b775a75a9962dc98584d0f3ebc451fb5d11d4f6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-24 12:16:59 -0500

    try repeating

--- cypress/e2e/settings.cy.ts
+++ cypress/e2e/settings.cy.ts
@@ -1,11 +1,32 @@
-it('opens the settings dialog', () => {
+import 'cypress-if';
+
+before(() => {
   cy.visit('/');
+});
+
+function ensureSettingsOpen() {
+  cy.get('dialog.settings', { timeout: 100 })
+    .if('not.visible')
+    .get('button.toggle-settings')
+    .click();
+  cy.get('dialog.settings').should('be.visible');
+}
+
+it('opens the settings dialog', () => {
   cy.get('dialog.settings').should('not.be.visible');
   cy.get('button.toggle-settings').click();
   cy.get('dialog.settings')
     .should('be.visible')
     // wait a second to make the dialog visible to the user
-    .wait(1000, { log: false });
-  cy.get('button.toggle-settings').click();
-  cy.get('dialog.settings').should('not.be.visible');
+    .wait(1000, { log: false })
+    // maybe validate something inside the dialog
+    .contains('Settings');
+});
+
+Cypress._.times(10, () => {
+  it('closes the settings dialog', () => {
+    ensureSettingsOpen();
+    cy.get('button.toggle-settings').click();
+    cy.get('dialog.settings').should('not.be.visible');
+  });
 });
